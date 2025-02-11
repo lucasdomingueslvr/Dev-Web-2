10816dd4114cfc604111e0087f849f9f0bb74f8e
lucasdomingueslvr <130410085+lucasdomingueslvr@users.noreply.github.com>
2025-02-11 01:05:35 +0000

    disciplina

--- aula-00-Revisao-POD/Disciplina.java
+++ aula-00-Revisao-POD/Disciplina.java
@@ -11,10 +11,33 @@ public class Disciplina {
     }
 
     public Disciplina(String nomeDisciplina) {
-
+        this(nomeDisciplina, new ArrayList<Aluno>(), new ArrayList<Professor>());
     }
 
     public Disciplina(String nomeDisciplina, List<Alunos> alunos, List<Professor> professores) {
 
+        this.nomeDisciplina = nomeDisciplina;
+        this.alunos = alunos;
+        this.professores = professores;
     }
+        public void setNomeDisciplina ( String nomeDisciplina){
+            this.nomeDisciplina = (nomeDisciplina);
+        }
+        public String getnomeDisciplina(){
+            return this.nomeDisciplina;
+        }
+        public void setAlunos(List<Alunos> alunos){
+        this.alunos = alunos;
+        }
+        public List<Aluno> getALunos(){
+            return this.alunos;
+        }
+        public void setProfessores(List<Professor> professores){
+            this.professores = professores;
+        }
+        public List<Professor> getProfessores(){
+            return this.professores;
+        }
+        
 }
+
