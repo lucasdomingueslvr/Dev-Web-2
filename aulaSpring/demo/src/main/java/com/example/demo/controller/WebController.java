2db22b09f22348115e5609ff7ae3f9847bea7788
lucasdomingueslvr <130410085+lucasdomingueslvr@users.noreply.github.com>
2025-04-15 00:26:15 +0000

    cont

--- aulaSpring/demo/src/main/java/com/example/demo/controller/WebController.java
+++ aulaSpring/demo/src/main/java/com/example/demo/controller/WebController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class WebController{
     @GetMaping("/")
     public String home(Model model){
-        
+        model.addAttribute("message", "teste");
+        return "home";
     }
 }
