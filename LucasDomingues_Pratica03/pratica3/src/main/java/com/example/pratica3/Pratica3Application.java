06e2a6c7a7c0518c151ef84078adc169111038c6
lucasdomingueslvr <130410085+lucasdomingueslvr@users.noreply.github.com>
2025-05-21 22:32:21 +0000

    veiculos

---
+++ LucasDomingues_Pratica03/pratica3/src/main/java/com/example/pratica3/Pratica3Application.java
@@ -0,0 +1,13 @@
+package com.example.pratica3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pratica3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pratica3Application.class, args);
+	}
+
+}
